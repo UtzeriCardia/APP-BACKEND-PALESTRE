31d7dd5e246e87b4c01f810c001749a83dbfd13e
Fabio Utzeri <fabioutzeri@MacBook-Pro-di-Fabio.local>
2021-11-02 18:09:03 +0100

    clean project

--- src/api/auth/auth.response.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "src/model/user";
-
-export interface IdentityResponse {
-    user: User;
-    tokenHash: string;
-}
--- src/config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    VERSION: '1.0',
-    ADMIN_KEYS: [
-        'H7UYYYsclS8RGWO1qf0z1cB1m73wF9FLl9RCc1Q', 
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
-    ]
-}
